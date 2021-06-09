40691b310025371b26f6b10af30971e194295533
AnaArroyoH <75263257+AnaArroyoH@users.noreply.github.com>
2021-06-09 16:31:28 +0200

    Add files via upload

---
+++ 23_Functions_basics/script.js
@@ -0,0 +1,29 @@
+
+//Defining a function
+const makingPizza = function () {
+    console.log("Make the pizza dough flat and round");
+    console.log("Put tomato sacuce on the flat dough");
+    console.log("Add the ingredients you like");
+    console.log("Place pizza in the oven");
+};
+
+//run the code, are you getting the result you expect? Why (not)?
+// I haven't called the function yet so nothing happend when running the code
+
+//Calling a function
+//makingPizza();
+
+//Defining a new fucntion
+const makingSushi = function () {
+    console.log("Spread some rice on the seaweed");
+    console.log("Put the ingredients you like in the middle on top of the rice");
+    console.log("Roll the seaweed keeping all ingredients inside");
+    console.log("Slice the roll and serve");
+}
+
+//Calling functions: 3x sushi and 2x pizza
+makingSushi();
+makingSushi();
+makingSushi();
+makingPizza();
+makingPizza();
